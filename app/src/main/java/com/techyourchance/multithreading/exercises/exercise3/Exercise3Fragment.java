8cca15446af2cace487c76f13fac4ac84707eabc
Pravin Tripathi <pravinyo12@gmail.com>
2021-01-19 21:32:14 +0530

    using handler to update ui from background thread

--- app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
+++ app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
@@ -1,6 +1,9 @@
 package com.techyourchance.multithreading.exercises.exercise3;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Exercise3Fragment extends BaseFragment {
 
     private static final int SECONDS_TO_COUNT = 3;
@@ -24,6 +29,7 @@ public class Exercise3Fragment extends BaseFragment {
 
     private Button mBtnCountSeconds;
     private TextView mTxtCount;
+    private final Handler mUIHandler = new Handler(Looper.getMainLooper());
 
     @Nullable
     @Override
@@ -33,12 +39,7 @@ public class Exercise3Fragment extends BaseFragment {
         mBtnCountSeconds = view.findViewById(R.id.btn_count_seconds);
         mTxtCount = view.findViewById(R.id.txt_count);
 
-        mBtnCountSeconds.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                countIterations();
-            }
-        });
+        mBtnCountSeconds.setOnClickListener(v -> countIterations());
 
         return view;
     }
@@ -48,6 +49,7 @@ public class Exercise3Fragment extends BaseFragment {
         return "Exercise 3";
     }
 
+    @SuppressLint("DefaultLocale")
     private void countIterations() {
         /*
         1. Disable button to prevent multiple clicks
@@ -55,5 +57,22 @@ public class Exercise3Fragment extends BaseFragment {
         3. Show count in TextView
         4. When count completes, show "done" in TextView and enable the button
          */
+        mBtnCountSeconds.setEnabled(false);
+        new Thread(() -> {
+            for (int i = 0; i < SECONDS_TO_COUNT ; i++) {
+                final int counter = i;
+                mUIHandler.post(() -> {
+                   mTxtCount.setText(String.format("Counter: %d", counter));
+                });
+
+                try { Thread.sleep(1000);}
+                catch (InterruptedException e) { e.printStackTrace(); }
+            }
+
+            mUIHandler.post(() -> {
+                mTxtCount.setText("done");
+                mBtnCountSeconds.setEnabled(true);
+            });
+        }).start();
     }
 }
